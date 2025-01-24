92dddce997d5eeceeb20cbb5a0567831b0914db3
Ali Ahmed ElKaramany <aliahmed999416@gmail.com>
2025-01-24 17:30:20 +0200

    Initial commit

---
+++ src/exercises/chapter7/DaysOfTheWeek.java
@@ -0,0 +1,14 @@
+package exercises.chapter7;
+
+/*
+ * The DayOfTheWeek program asks the user to enter a number representing a day of the week,
+ * then outputs the corresponding day name. The program assumes the user enters a number between 1 and 7.
+ * Monday is the first day of the week.
+ */
+
+public class DaysOfTheWeek {
+
+    public static void main(String[] args){
+
+    }
+}
