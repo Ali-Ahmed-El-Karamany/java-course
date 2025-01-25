b4e524482fdd8b193d8aac8d9a8e6cec11db81b0
Ali Ahmed ElKaramany <aliahmed999416@gmail.com>
2025-01-25 17:11:34 +0200

    Add Employee class that inherits for Person Class

---
+++ src/chapter9/Employee.java
@@ -0,0 +1,48 @@
+package chapter9;
+
+/**
+ * The Employee class inherits from the Person class, adding additional attributes specific to an employee.
+ * It represents an employee with details such as their employee ID and job title.
+ */
+public class Employee extends Person {
+
+    // Private fields to store employee's specific attributes
+    private String employeeId; // The unique identifier for the employee
+    private String title;      // The job title of the employee
+
+    /**
+     * Gets the employee ID of the employee.
+     *
+     * @return the employee's unique identifier.
+     */
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    /**
+     * Sets the employee ID of the employee.
+     *
+     * @param employeeId the unique identifier to assign to the employee.
+     */
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    /**
+     * Gets the job title of the employee.
+     *
+     * @return the employee's job title.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the job title of the employee.
+     *
+     * @param title the job title to assign to the employee.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
