683b353d1d009e37412a93bc89758d55da7875ac
Ali Ahmed ElKaramany <aliahmed999416@gmail.com>
2025-01-25 17:19:21 +0200

    Add InheritesTester class to test inheritance concepts

---
+++ src/chapter9/InheritanceTester.java
@@ -0,0 +1,15 @@
+package chapter9;
+
+/**
+ * The InheritanceTester class designed to test and demonstrate
+ * the concepts of inheritance using the Person and Employee classes.
+ * It serves as the entry point for running inheritance-related examples.
+ */
+
+public class InheritanceTester {
+
+    public static void main(String[] args) {
+
+
+    }
+}
